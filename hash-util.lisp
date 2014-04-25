2ff54e8cfa6e407c50829d9e106bbe2af7c36ed6
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-04-24 18:18:39 -0700

    renaming hash-get to hget and "fixing" docstrings to be indented "properly"

--- hash-util.lisp
+++ hash-util.lisp
@@ -5,10 +5,10 @@
 ;;;   (gethash "city" (gethash "location" (gethash "user" obj)))
 ;;;
 ;;; With cl-hash-util, you can write:
-;;;   (hash-get obj '("user" "location" "city"))
+;;;   (hget obj '("user" "location" "city"))
 ;;;
-;;; hash-get can also deal with getting elements out of lists and arrays:
-;;;   (hash-get obj '("user" "friends" 0 "name"))
+;;; hget can also deal with getting elements out of lists and arrays:
+;;;   (hget obj '("user" "friends" 0 "name"))
 ;;;
 ;;; which normally would have to be written as such:
 ;;;   (gethash "name" (elt (gethash "friends" (gethash "user" obj)) 0))
@@ -43,7 +43,7 @@
   (:use :cl :cl-user)
   (:export hash-create
            hash
-           hash-get
+           hget
            hash-copy)
   (:nicknames :hu))
 (in-package :cl-hash-util)
@@ -51,16 +51,16 @@
 (defun hash-create (pairs &key (test #'equal))
   "Create a hash table with limited syntax:
 
-    (hash `((\"name\" \"andrew\") (\"city\" \"santa cruz\")))
-  
-  which would otherwise be:
-  
-    (let ((hash (make-hash-table :test #'equal)))
-      (setf (gethash \"name\" hash) \"andrew\")
-      (setf (gethash \"city\" hash) \"santa cruz\")
-      hash)
-  
-  yuck city."
+     (hash `((\"name\" \"andrew\") (\"city\" \"santa cruz\")))
+
+   which would otherwise be:
+
+     (let ((hash (make-hash-table :test #'equal)))
+       (setf (gethash \"name\" hash) \"andrew\")
+       (setf (gethash \"city\" hash) \"santa cruz\")
+       hash)
+
+   yuck city."
   (let ((hash (make-hash-table :test test)))
     (dolist (pair pairs)
       (setf (gethash (car pair) hash) (cadr pair)))
@@ -70,16 +70,16 @@
   "Extends hash-create syntax to make it nicer."
   `(hash-create (list ,@(loop for pair in pairs collect (list 'list (car pair) (cadr pair))))))
 
-(defun hash-get (obj path)
+(defun hget (obj path)
   "Allows you to specify a path to get values out of a hash/list object. For
-  instance, if you did:
+   instance, if you did:
 
-    (let ((myhash (hash '(\"lol\" '(3 4 5)))))
-      (hash-get myhash '(\"lol\" 1)))
+     (let ((myhash (hash '(\"lol\" '(3 4 5)))))
+       (hget myhash '(\"lol\" 1)))
 
-  which would return 4 (1st index of list stored under key 'lol of the hash
-  table). Simplifies traversing responses from decoded JSON objects by about a
-  trillion times."
+   which would return 4 (1st index of list stored under key 'lol of the hash
+   table). Simplifies traversing responses from decoded JSON objects by about a
+   trillion times."
   (let ((placeholder obj))
     (dolist (entry path)
       (setf placeholder (if (numberp entry)
@@ -87,11 +87,11 @@
                             (gethash entry placeholder))))
     placeholder))
 
-(defun (setf hash-get) (val obj path)
-  "Defines a setf for the hash-get function. Uses hash-get to get all but the
-  last item in the path, then setfs that last object (either a gethash or an
-  elt)."
-  (let ((last-obj (hash-get obj (butlast path)))
+(defun (setf hget) (val obj path)
+  "Defines a setf for the hget function. Uses hget to get all but the
+   last item in the path, then setfs that last object (either a gethash or an
+   elt)."
+  (let ((last-obj (hget obj (butlast path)))
         (final (car (last path))))
     (if (numberp final)
         (setf (elt last-obj final) val)
@@ -103,6 +103,6 @@
   (let ((new-hash (make-hash-table :test test :size (hash-table-count hash))))
     (loop for k being the hash-keys of hash
           for v being the hash-values of hash do
-          (setf (hash-get new-hash (list k)) (hash-get hash (list k))))
+          (setf (hget new-hash (list k)) (hget hash (list k))))
     new-hash))
 
