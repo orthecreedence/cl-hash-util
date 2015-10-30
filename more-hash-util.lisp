e97ab353c3b4b1227e5faf81a58ee477f120764a
Ben McGunigle <bnmcgn@gmail.com>
2015-10-30 07:31:29 -0700

    Added collecting-hash-table.

--- more-hash-util.lisp
+++ more-hash-util.lisp
@@ -29,3 +29,45 @@
                                  ,htsym))))
              keys)
          ,@body))))
+
+(defun %hash-collecting-modes (mode)
+  (case mode
+    (:replace (list
+               (lambda (e n) (declare (ignore e)) n)
+               (lambda (v) v)))
+    (:keep (list
+            (lambda (e n) (declare (ignore n)) e)
+            (lambda (v) v)))
+    (:tally (list
+             (lambda (e n) (declare (ignore n)) (1+ e))
+             (lambda (v) (declare (ignore v)) 1)))
+    (:sum (list
+           (lambda (e n) (+ e n))
+           (lambda (v) v)))
+    (:append (list
+              (lambda (e n) (nconc e (list n)))
+              (lambda (v) (list v))))
+    (:push (list
+            (lambda (e n) (cons n e))
+            (lambda (v) (list v))))
+    (otherwise (error "Mode not found"))))
+
+(defmacro collecting-hash-table ((&key (test '(function eql) test-set-p)
+                                    existing (mode :append)) &body body)
+  (and test-set-p existing
+       (error "Can't set test when reusing an existing hash table"))
+  (let ((fill (gensym))
+        (init (gensym))
+        (stor (gensym)))
+    `(let ((,stor (aif ,existing it (make-hash-table :test ,test))))
+       (destructuring-bind (,fill ,init) (%hash-collecting-modes ,mode)
+         (labels
+             ((collect (key value &key mode)
+                (destructuring-bind (fill init)
+                    (if mode (%hash-collecting-modes mode) (list ,fill ,init))
+                  (multiple-value-bind (itm exists) (gethash key ,stor)
+                    (if exists
+                        (setf (gethash key ,stor) (funcall fill itm value))
+                        (setf (gethash key ,stor) (funcall init value)))))))
+           ,@body
+           ,stor)))))
