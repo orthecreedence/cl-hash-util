cecaa89776013f49f982062258ef799a466da491
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-06-28 05:55:08 -0700

    Added some tests for (setf hget)

--- tests.lisp
+++ tests.lisp
@@ -18,6 +18,16 @@
       (hget ht '(c d))))
   (is (= 3 (let ((*error-on-nil* t))
              (hget ht '(a b)))))
-  (signals type-error (setf (hget ht '(b c)) t))
+  (signals simple-error (setf (hget ht '(b c)) t)))
+
+(test setf-hget
   (is (= 4 (setf (hash-get ht '(a c)) 4)))
-  (is (= 4 (hget ht '(a c)))))
+  (is (= 4 (hget ht '(a c))))
+  (signals simple-error
+    (setf (hget ht '(c d)) 5))
+  (signals simple-error
+    (let ((*error-on-nil* t))
+      (setf (hget ht '(c d)) 5)))
+  (is (equal '(b c d) (nth-value 2 (hget ht '(b c d)))))
+  (is (= 5 (setf (hget ht '(b c d) :fill-func #'make-hash-table) 5)))
+  (is (= 5 (hget ht '(b c d)))))
